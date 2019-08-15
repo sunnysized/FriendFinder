021f8805a36d51458495543a08b44856a27fc285
Sonal Patel <sonalpatel.sunny@gmail.com>
2019-08-15 07:53:26 -0500

    resubmit

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+      name: 'Sonal Patel',
+      profilePic: 'https://avatars1.githubusercontent.com/u/47607367?s=400&u=4318d9db320de127bd8c5da413533fdddf5972ab&v=4',
+      scores: [
+        "5",
+        "3",
+        "4",
+        "2",
+        "5",
+        "2",
+        "4",
+        "1",
+        "4",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
